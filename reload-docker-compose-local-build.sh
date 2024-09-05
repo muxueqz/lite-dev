b0f19cfa1fbc3dd40ae223f98ae7e40f6e77ebfc
muxueqz <zhangmingyuan240@gmail.com>
2024-09-06 00:09:34 +0800

    custom compose cmd

--- reload-docker-compose-local-build.sh
+++ reload-docker-compose-local-build.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 echo Reloading Docker compose
+if [ -z ${COMPOSE_FILE} ]; then
+  COMPOSE_FILE='docker-compose.yml'
+fi
+COMPOSE_CMD="podman_compose.py -f ${COMPOSE_FILE}"
+echo ${COMPOSE_CMD}
 # podman_compose.py down -t 1
 # podman_compose.py down -t 1
 sleep 0.5s
 $@
 if [[ $? == 0 ]]; then
-  podman_compose.py restart
+  # podman_compose.py -f ${COMPOSE_FILE} restart
+  ${COMPOSE_CMD} restart -t 0
   echo "my exit:" $?
   # flock --unlock ${lock_file} echo
   # flock --close ${lock_file} podman_compose.py logs -f &
   rm -rfv ${lock_file}
-  podman_compose.py logs -f &
+  ${COMPOSE_CMD} logs -f &
   # podman_compose.py up &
 fi
